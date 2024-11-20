5fbaa67e546c788043ce9779d7c61158710cf8f7
Chetopsa <125150503+Chetopsa@users.noreply.github.com>
2024-11-20 13:19:52 -0600

    added some comments

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ require('dotenv').config();
 // include the express modules
 var express = require("express");
 var app = express();
-const path = require('path');
-const { Pool } = require('pg');
 
-var session = require('express-session');
-const passport = require('./passport'); // Import the passport configuration
+var session = require('express-session'); // import session to keep track of users
+const passport = require('./passport'); // import the passport configuration
 
 // use sequelize orm for database
 const db = require("./models");
 const {User, Meal, RSVP} = require('./models');
 
 app.use(express.json()); // middleware for parsing
-app.use(session({ // middleware for storing user id
+app.use(session({ // middleware for storing user info
     secret: "pooperscooper",
     saveUninitialized: true,
     resave: false,
@@ -25,12 +23,13 @@ app.use(session({ // middleware for storing user id
 ));
 app.use(passport.initialize());
 app.use(passport.session());
+// middleware function used in protected routes to ensure user is logged in
 function isAuthenticated(req, res, next) {
     if (req.session.isAuthenticated) {
-        // If user is authenticated, proceed to the next middleware/route
+        // if user is authenticated, proceed 
         return next();
     }
-    // If not authenticated, send an error or redirect to login page
+    // send error if not authenticated
     res.status(401).send('Unauthorized: You must log in to access this resource.');
 }
 
@@ -42,9 +41,9 @@ app.get('/', (req, res) => {
 // google auth route
 app.get('/auth/google', passport.authenticate('google', {scope: ['email']}));
 
-// Define the callback route for Google
+// define the callback route for Google
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    // Successful authentication, redirect home or any other page.
+    // successful authentication, redirect to another page
     req.session.isAuthenticated = true
     console.log(req.user.userID);
     if (req.user.firstName == null) {
@@ -57,12 +56,13 @@ app.get('/auth/google/callback', passport.authenticate('google', { failureRedire
   }
 );
 // all routes
+// just a bunch of testing routes
 app.get('/profile', isAuthenticated, (req, res) => {
   
-  res.json({ message: `Welcome ${req.user.firstName}` });
+  res.json({ message: `Welcome ${req.user.googleID}` });
 });
 
-
+// is Autthenticated middleware used to protect routes if user is not signed in
 app.get('/select', isAuthenticated, (req, res) => {
     User.findOne( {where: {firstName: "chet"}})
     .then((users) => {
